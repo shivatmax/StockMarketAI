dcee29bcfaf8c557ed6fa34ecb21643565c827a2
shivatmax <awasthishiv0987@gmail.com>
2024-01-17 20:52:29 +0530

    12

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+import os
+import shutil
 import time
 import streamlit as st
 from crewai import Task, Crew
@@ -6,13 +7,12 @@ import Agents.crew_agents as agents
 import datetime
 from clear_pycache import clear_pycache
 import tools.bingimage as bi
-
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 # Add image to Streamlit app
 image_path = "images/stock4-remove.png"
-st.image(image_path, use_column_width=True, caption="Stocky AI",width=500)
-
-
+st.image(image_path, use_column_width=True, caption="Stocky AI", width=500)
 
 # Add custom CSS styles
 st.markdown(
@@ -110,19 +110,28 @@ task3 = Task(
         You MUST Consider all aspects, including Recent News,Upcoming Events,Potential Impacts on the Stock,Key financial
         health and metrics, market sentiment, and qualitative data from
         EDGAR filings with statistics, percentages, prices and numbers.
-
+       
+        YOU MUST ADD new line charater '\ n' to the end of every full stop '.'
+       
         Make sure to include a section that shows insider 
         trading activity, and upcoming events like earnings.
+        
+        YOU MUST ADD new line charater '\ n' to the end of every full stop '.'
 
         Your final answer MUST be a recommendation with rating out of 10 for your
         customer.
         It should be a full super detailed report, providing a 
         clear investment stance and strategy with supporting evidence.
-
+        
+        YOU MUST ADD new line charater '\ n' to the end of every full stop '.'
+        
         Your Final Answer MUST be a detailed report with bullet points that includes all aspects, including Recent News, 
         Negative News, Positive News, Upcoming Events, Potential Impacts on the Stock, Key financial
         health and metrics, market sentiment, and qualitative data with statistics, percentages, prices and numbers.
+        
         Make it pretty and well formatted for your customer.
+        
+        YOU MUST ADD new line charater '\ n' to the end of every full stop '.'
       """,
     agent=agents.investment_Advisor,
 )
@@ -135,25 +144,27 @@ crew = Crew(
         agents.investment_Advisor,
     ],
     tasks=[task1, task2, task3],
-    verbose=3,  # You can set it to 1 or 2 for different logging levels
+    verbose=False,  # You can set it to 1 or 2 for different logging levels
 )
 
 # Add custom styling to the app
 st.markdown("<h1 class='title'>Stocky AI!!</h1>", unsafe_allow_html=True)
-st.markdown("<p class='subtitle'>Welcome to the AI Stock Researcher and Analyst! 😄</p>", unsafe_allow_html=True)
+st.markdown("<p class='subtitle'>Hi !! I'm Stocky, the AI Stock Researcher and Analyst! 😄</p>", unsafe_allow_html=True)
 
 # Get user input for the topic
 topic = st.text_input("Enter the Name of Company")
 
 # Check if the topic is provided
 if topic:
-    company_logo= bi.CompanyLogo(topic)
-    time.sleep(2)
-    
+    logo = bi.CompanyLogo(topic)
+    company_logo = str(logo)
+
+    time.sleep(1)
+
     # Update the task descriptions with the user-provided topic
     task1.description = task1.description.format(Topic=topic)
     task2.description = task2.description.format(Topic=topic)
-    
+
     col1, col2, col3 = st.columns([1, 0.5, 1])
     with col1:
         st.image(company_logo, caption=topic + " Company", width=200)
@@ -162,18 +173,57 @@ if topic:
     with col3:
         st.write("")
         st.write("")
-        st.write("")
         a = st.button(f"Generate {topic} stock report", key="generate_report")
+        st.write("")
+        # 
     if a:
         with st.spinner(f"Generating {topic} stock report..."):
             result = crew.kickoff()
+
             st.markdown("<h2 class='subtitle'>Stock Report</h2>", unsafe_allow_html=True)
-            st.write(result)
-            time.sleep(40)
-            # Call the function to clear __pycache__ folders
+            t1 = result.replace("**", " ")
+            text1 = t1.replace(" *", "•")
+            st.write(text1)
+            time.sleep(2)
+
+            print(text1)
+
+            # Create a PDF canvas
+            from reportlab.lib.units import inch
+            c = canvas.Canvas(f"{topic}_stock_report.pdf", pagesize=letter)
+            c.setPageSize((15 * inch, 11.7 * inch))
+
+            w, h = letter
+            c.drawImage(company_logo, x=100, y=h - 50, width=100, height=75)
+
+            # Write the result text below the company logo
+            c.setFont("Helvetica", 8)
+            text = c.beginText(20, h - 60)
+            text.textLines(str(text1))
+            c.drawText(text)
+            c.showPage()
+            # Save and close the PDF canvas
+            c.save()
+
+            def openfile(topic):
+                with open(f"{topic}_stock_report.pdf", "rb") as file:
+                    return file.read()
+
+            # Provide a download button for the PDF file
+            st.download_button(
+                label="Download Stock Report",
+                data=openfile(topic),
+                file_name=f"{topic}_stock_report.pdf",
+                mime="application/pdf"
+            )
+
+            def clear_images_folder():
+                folder_path = "images/company"
+                shutil.rmtree(folder_path)
+
+            clear_images_folder()
+            time.sleep(10)
+            os.remove(f"{topic}_stock_report.pdf")
             clear_pycache()
 else:
-    st.write("Please enter a Company Name to generate a stock report.Like Tata Motors")
-    
-
-
+    st.write("Please enter a Company Name to generate a stock report. Like Tata Motors")
--- tools/bingimage.py
+++ tools/bingimage.py
@@ -1,15 +1,21 @@
-
 from bing_image_downloader import downloader
 import os
 from PIL import Image
 
 def CompanyLogo(Topic):
     querystring = f"{Topic} Company logo"
-    downloader.download(querystring, limit=1,  output_dir='images\company', adult_filter_off=True, force_replace=True, timeout=10, verbose=True)
+    downloader.download(querystring, limit=1,  output_dir='images\company', adult_filter_off=True, force_replace=True, timeout=20, verbose=True)
+    
+    downloaded_images = os.listdir(f'images\company\{Topic} Company logo')
+    # print(downloaded_images)
+    if len(downloaded_images) == 0:
+        print("No images were downloaded. Trying again...")
+        downloader.download(querystring, limit=1,  output_dir='images\company', adult_filter_off=True, force_replace=True, timeout=20, verbose=True)
+        downloaded_images = os.listdir(f'images\company\{Topic} Company logo')
     
     # Rename the downloaded image to "image.png"
     image_path = os.path.join('images\company', f"{Topic} Company logo")
-    print(image_path)
+    # print(image_path)
     new_image_path = None  # Initialize new_image_path variable
     for file in os.listdir(image_path):
         if file.startswith('Image_'):
@@ -22,5 +28,7 @@ def CompanyLogo(Topic):
     if new_image_path:
         image = Image.open(new_image_path)
         image.save(new_image_path, 'PNG')
+    else:
+        return "images\default\Company_logo.png"
     
     return new_image_path
