59105820f4e79a4f411bac2e7aeaf4a4fd3b1b88
Shiv Awasthi <115219800+shivatmax@users.noreply.github.com>
2024-01-17 23:33:59 +0530

    Update bingimage.py

--- tools/BingDownloader/bingimage.py
+++ tools/BingDownloader/bingimage.py
@@ -1,20 +1,19 @@
 from tools.BingDownloader import downloader
 import os
 from PIL import Image
+
 def CompanyLogo(Topic):
     querystring = f"{Topic} Company logo"
-    downloader.download(querystring, limit=1,  output_dir='images\company', adult_filter_off=True, force_replace=True, timeout=20, verbose=True)
+    downloader.download(querystring, limit=1,  output_dir='images/company', adult_filter_off=True, force_replace=True, timeout=20, verbose=True)
     
-    downloaded_images = os.listdir(f'images\company\{Topic} Company logo')
-    # print(downloaded_images)
+    downloaded_images = os.listdir(f'images/company/{Topic} Company logo')
     if len(downloaded_images) == 0:
         print("No images were downloaded. Trying again...")
-        downloader.download(querystring, limit=1,  output_dir='images\company', adult_filter_off=True, force_replace=True, timeout=20, verbose=True)
-        downloaded_images = os.listdir(f'images\company\{Topic} Company logo')
+        downloader.download(querystring, limit=1,  output_dir='images/company', adult_filter_off=True, force_replace=True, timeout=20, verbose=True)
+        downloaded_images = os.listdir(f'images/company/{Topic} Company logo')
     
     # Rename the downloaded image to "image.png"
-    image_path = os.path.join('images\company', f"{Topic} Company logo")
-    # print(image_path)
+    image_path = os.path.join('images/company', f"{Topic} Company logo")
     new_image_path = None  # Initialize new_image_path variable
     for file in os.listdir(image_path):
         if file.startswith('Image_'):
@@ -28,6 +27,6 @@ def CompanyLogo(Topic):
         image = Image.open(new_image_path)
         image.save(new_image_path, 'PNG')
     else:
-        return "images\default\Company_logo.png"
+        return "images/default/Company_logo.png"
     
     return new_image_path
